9a908c9c7d94543ca2a4a7c9f09debc8078cc00d
Ryan Vahora <ryanvnews@gmail.com>
2026-01-04 19:28:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var navLinks = document.getElementById("nav-links");
+
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
+
+function showMenu() {
+    navLinks.style.right = "0";
+}
+
